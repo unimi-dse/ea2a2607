64350bacaab8e3ad750fb02f9ffdb594b9528abd
GLV-28 <59977851+GLV-28@users.noreply.github.com>
2020-02-07 12:46:53 +0100

    Cleaner

--- R/hello.R
+++ R/hello.R
@@ -1,29 +1,7 @@
 
 
 
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
 
-
-require(tidyr)
-library(reshape2)
-library(ggplot2)
-library(dplyr)
-library(devtools)
-library(roxygen2)
-require(Hmisc)
-require(foreach)
-require(lessR)
-require(rlist)
-require(marray)
 
 
 
@@ -71,25 +49,17 @@ Regmaker=function(){
     DatasetEx=as.data.frame(DatasetEx)
     x= DatasetEx[,i] ~. -DatasetEx[,i]
     model=lm(DatasetEx[,i] ~. -DatasetEx[,i], DatasetEx)
-    #summary(model)
-    #anova(model)
-     #options(warn=0)
     d=list(suppressWarnings(summary(model)))
     print(d)
     b=list(suppressWarnings(anova(model)))
-    #print(b)
-    reg_list[[i]] = model#,d,b #lm(my_formula, data = data)
+    print(b)
+    reg_list[[i]] = model
     list.append(reg_list,d)
     list.append(reg_list,b)
-    #list.append(Petardo,d,b)
+
     }
   save(reg_list, file="Results.rda")
-  return(#Results=
-    #save(reg_list, file="Results")
-    #lapply( reg_list, cat,"\n", file="Results.txt", append=TRUE
-     #write.list(reg_list, filename = "Results", #append = FALSE, closefile = TRUE, Results
-
-  View(reg_list, "Results"))
+  return(View(reg_list, "Results"))
 }
 
 
